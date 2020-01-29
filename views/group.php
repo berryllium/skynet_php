bd538e6de48fd61da27d27078fb3f8980054283f
berryllium <gorkundp@yandex.ru>
2020-01-29 17:02:12 +0300

    hover params

--- views/group.php
+++ views/group.php
@@ -10,7 +10,7 @@
         <header class="card__header">
           <?= $tarif->pay_period . ' ' . Help::suffixMonth($tarif->pay_period) ?>
         </header>
-        <div onclick="getPage('index.php?page=tarif&group=<?= $groupID ?>&id=<?= $tarif->ID ?>')" class="card__param">
+        <div onclick="getPage('index.php?page=tarif&group=<?= $groupID ?>&id=<?= $tarif->ID ?>')" class="card__param  card__param_link">
           <div class="card__arrow">&rsaquo;</div>
           <div class="card__price"><?= $tarif->price / $tarif->pay_period ?> &#8381;/мес</div>
           <div class="card__payment">разовый платеж &ndash; <?= $tarif->price ?> &#8381;</div>
--- views/main.php
+++ views/main.php
@@ -5,7 +5,7 @@ foreach ($model->data as $id => $tarif) :?>
     <header class="card__header">
       Тариф "<?= $tarif->title ?>"
     </header>
-      <div onclick="getPage('index.php?page=group&group=<?= $id ?>')" class="card__param">
+      <div onclick="getPage('index.php?page=group&group=<?= $id ?>')" class="card__param card__param_link">
         <div class="card__arrow">&rsaquo;</div>
         <div class="card__speed card__speed_<?= $model->getMod($tarif->title) ?>"><?= $tarif->speed ?> Мбит/с</div>
         <div class="card__price"><?= $model->compPrice($id, 'min'); ?> &ndash; <?= $model->compPrice($id, 'max'); ?> &#8381;/мес</div>
